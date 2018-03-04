cf583176f2eb0e32e686b8514d19bd3fa6f3cec6
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-03-03 22:47:32 -0300

    VSCode installer

---
+++ vscode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear
+echo "========================================================="
+echo "Baixando última versão estável amd64 em /var/tmp"
+echo "========================================================="
+cd /var/tmp
+wget -O vscode.deb https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
+
+echo "========================================================="
+echo "Desinstalando versões anteriores"
+echo "========================================================="
+sudo apt-get purge vscode
+sudo apt-get autoremove
+sudo apt-get autoclean
+
+echo "========================================================="
+echo "Instalando nova versão"
+echo "========================================================="
+sudo dpkg -i vscode.deb
+
+echo "========================================================="
+echo "Instalando dependências"
+echo "========================================================="
+sudo apt-get -f -y install
+
+echo "========================================================="
+echo "Removendo instalador"
+echo "========================================================="
+sudo rm vscode.deb
